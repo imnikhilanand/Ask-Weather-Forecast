a822fef9d01b7ded5a42c8dc9f1f0add8179de3c
Nikhil Anand <imnikhilanand@gmail.com>
2018-05-09 10:06:16 +0530

    Add files via upload

--- home.py
+++ home.py
@@ -1,20 +1,42 @@
-import speech_recongnition as sr
-import re 
-import nltk
-nltk.download('stopwords')
-from nltk.stem.porter import PorterStemmer
-from nltk.corpus import stopwords
-from urllib.request import urlopen
+#library for json 
+
+from urllib2 import urlopen
 import json
 import pyttsx
 
-speech_engine = pyttsx.init('espeak') 
-speech_engine.setProperty('rate', 150)
 
-def speak(text):
-    speech_engine.say(text)
-    speech_engine.runAndWait()
+#library for nltk
+import re 
+import nltk
+#nltk.download('stopwords')
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
+
+
+#library for speech recognition
+import speech_recognition as sr 
+r = sr.Recognizer()
+import sys
+
+#function t take voice input
+def bol_bhai():
+	with sr.Microphone() as source:
+		#print('Say Something')
+		audio = r.listen(source)
+
+	try:
+		abc = r.recognize_google(audio)
+
+	except:
+		pass	
+	return abc
+
+
+new_arr = []
+new_arr2 = []
+new_arr3 = []
+new_arr4 = []    
 
 def tell_weather(string):
     stop_words = set(stopwords.words('english')) 
@@ -22,11 +44,7 @@ def tell_weather(string):
     review = review.lower()
     review = review.split()
     ps = PorterStemmer()
-    new_arr = []
-    new_arr2 = []
-    new_arr3 = []
     
-
     for r in review:
         if not r in stop_words:
             new_arr.append(r)
@@ -37,48 +55,71 @@ def tell_weather(string):
         temp_words.add("pressure")
         temp_words.add("maximum")   
         temp_words.add("minimum")
-        
-
+    
     for m in new_arr:
         if not m in temp_words:
             new_arr2.append(m)        
         else:
             new_arr3.append(m)    
-
-    new_arr4 = []
-
+    	
     for x in new_arr3:
-        if x == "temperature":
-            new_arr4.append("temp") 
         if x == "maximum":
             new_arr4.append("temp_max")
         if x == "minimum":
             new_arr4.append("temp_min")
+        if x == "temperature":
+            new_arr4.append("temp") 
         if x == "pressure":
             new_arr4.append("pressure")
         if x == "humidity":    
             new_arr4.append("humidity")
+
+
+#speck engine to give speech output
+
+speech_engine = pyttsx.init('espeak') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init nsss for mac
+speech_engine.setProperty('rate', 150)
+
+def speak(text):
+    speech_engine.say(text)
+    speech_engine.runAndWait()
+
+
+
+#speak("Ask weather forecast")
+text=bol_bhai()
+#tell_weather("What is Patna maximum temperature")
+tell_weather(text)
+
+
+
+# Weather api to predict weather
+url = 'http://api.openweathermap.org/data/2.5/weather?appid=c140dd2e860dc97628ab3a2397aa6183&q='+new_arr2[0]
+response = urlopen(url)
+string = response.read().decode('utf-8')
+json_obj = json.loads(string)
+resu=json_obj['main'][new_arr4[0]]-273.15
     
-    # Weather api to predict weather
-    url = 'http://api.openweathermap.org/data/2.5/weather?appid=c140dd2e860dc97628ab3a2397aa6183&q='+new_arr2[0]
-    response = urlopen(url)
-    string = response.read().decode('utf-8')
-    json_obj = json.loads(string)
-    print(json_obj['main'][new_arr4[0]]-273.15)
-    return;
+
+p = str(resu)
+q = str(new_arr2[0])
+r = str(new_arr4[0])
+
+#print(r)
+#speak(r)
 
 
+if r == "temp_max":
+	speak(q+" maximum temperature is "+ p +" degree celcius")
+elif r == "temp_min":
+	speak(q+" minimum temperature is "+ p +" degree celcius")
+elif r == "humidity":
+	speak(q+" humidity is "+ p)
+elif r == "temp":
+	speak(q+" temperature is "+ p +" degree celcius")
+elif r == "pressure":
+	speak(q+" pressure is "+ p +" newton meter")
 
-r = sr.Recognizer();
-with sr.Microphone() as source:
-    speak("Hey I am here to tell you the weeather forecast, ask me")
-    audio = r.listen(source)
-try:
-    string = r.recognize_google(audio)
-    tell_weather(string = string2)
-except: 
-    pass
+sys.exit()
 
 
-    
-    
